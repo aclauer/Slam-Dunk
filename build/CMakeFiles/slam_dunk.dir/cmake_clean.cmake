50f40b2327e5432b12008faa5b52cc8a05d74190
Andrew Lauer <andrewcl@email.unc.edu>
2023-04-16 22:45:32 -0400

    updated gitignore

---
+++ build/CMakeFiles/slam_dunk.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/slam_dunk.dir/test.cpp.o"
+  "CMakeFiles/slam_dunk.dir/test.cpp.o.d"
+  "slam_dunk"
+  "slam_dunk.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/slam_dunk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
