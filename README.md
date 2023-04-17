4b806eae92781c6a600c992ea2da1b09a2fbb7fd
Andrew Lauer <andrewcl@email.unc.edu>
2023-04-16 23:12:32 -0400

    prepping to move to ubunutu

--- README.md
+++ README.md
@@ -9,5 +9,9 @@ cmake --build build
 ./build/slam_dunk
 ```
 
+# Dependencies I downloaded
+
+[macOS pre-compiled binaries](https://www.glfw.org/download.html)
+
 # Resources
 J. Behley and C. Stachniss, [“Efficient Surfel-Based SLAM using 3D Laser Range Data in Urban Environments,”](http://www.roboticsproceedings.org/rss14/p16.pdf) in Robotics: Science and Systems XIV, Robotics: Science and Systems Foundation, Jun. 2018. doi: 10.15607/RSS.2018.XIV.016.
--- test.cpp
+++ test.cpp
@@ -1,9 +1,50 @@
+#include <stdlib.h>
 #include <iostream>
+#define GL_SILENCE_DEPRECATION
+#include <GLFW/glfw3.h>
+#include <GLUT/glut.h>
+//#include <OpenGL/gl3.h>
 using namespace std;
 
-int main()
-{
-    cout << "Hello, World!!!\n";
-    cout << "THIS IS AWESOME\n";
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 400;
+
+int main() {
+    cout << "Starting...\n";
+
+    int major, minor, revision;
+    glfwGetVersion(&major, &minor, &revision);
+
+    // Output GLFW version number
+    std::cout << "GLFW version: " << major << "." << minor << "." << revision << std::endl;
+    
+
+    if (!glfwInit()) {
+        cout << "Error initializing glfw.";
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Test window", NULL, NULL);
+    if (!window) {
+        cout << "Error creating the window.\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    
+    glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+    //glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+    //glClear(GL_COLOR_BUFFER_BIT);
+    //glfwSwapBuffers(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        // Do stuff.
+
+        glfwPollEvents();
+    }
+
+    cout << "Time to be done!\n";
+    glfwTerminate();
     return 0;
 }
