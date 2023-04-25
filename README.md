290cf058fc478779de4316448f1463a6f02c52a2
Andrew Lauer <andrewcl@email.unc.edu>
2023-04-25 06:40:32 -0700

    Basic visualizer method implemented

--- README.md
+++ README.md
@@ -16,8 +16,11 @@ sudo apt-get install libglfw3
 sudo apt-get install libglfw3-dev
 sudo apt-get install libpcl-dev
 sudo apt-get install libpcap-dev
+sudo apt-get install pcl-tools
 
 ```
 
 # Resources
 J. Behley and C. Stachniss, [“Efficient Surfel-Based SLAM using 3D Laser Range Data in Urban Environments,”](http://www.roboticsproceedings.org/rss14/p16.pdf) in Robotics: Science and Systems XIV, Robotics: Science and Systems Foundation, Jun. 2018. doi: 10.15607/RSS.2018.XIV.016.
+
+[PCD Downloads](https://sourceforge.net/projects/pointclouds/files/)
--- main.cpp
+++ main.cpp
@@ -3,18 +3,70 @@
 #include <pcl/point_types.h>
 #include <pcl/visualization/cloud_viewer.h>
 #include <pcap.h>
+//#include <pcl/io/pcap_io.h>
+#include <pcl/point_cloud.h>
 
+/*
+pcl::PointCloud<pcl::PointXYZ>::Ptr pcapToPointCloud(const char* path) {
+    pcl::PCapReader<pc1::PointXYZ> reader;
+    reader.open(path);
 
-pcl::PointCloud<pcl::PointXYZ>::Ptr pcap_to_point_cloud(const char* path) {
-    //char errbuf[PCAP_ERRBUF_SIZE];
+    if (!reader.isOpen()) {
+        std::cerr << "Failed to open pcap file: " << path << std::endl;
+        return NULL;
+    }
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pc1::PointXYZ>());
     
-}
+    while (!reader.wasStopped()) {
+        pcl::PointXYZ point;
 
+        if (reader.readPacket(point)) {
+            cloud->push_back(point);
+        } else {
+            break;
+        }
+    }
+
+    return cloud;
+}
+*/
+/*
+pcl::PointCloud<pcl::PointXYZ>::Ptr pcapToPointCloud(const std::string& pcapPath) {
+    // Load the pcap file
+    pcl::PCapReader<pcl::PointXYZ> reader;
+    reader.open(pcapPath);
+
+    if (!reader.isOpen()) {
+        std::cerr << "Failed to open pcap file: " << pcapPath << std::endl;
+        return nullptr;
+    }
+
+    // Create a point cloud object
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+
+    // Loop through each packet in the pcap file
+    while (!reader.wasStopped()) {
+        pcl::PointXYZ point;
+
+        // Read the next packet
+        if (reader.readPacket(point)) {
+            // Add the point to the point cloud
+            cloud->push_back(point);
+        } else {
+            break;  // Break the loop when no more packets are available
+        }
+    }
+
+    return cloud;
+}
+*/
 
 int main() {
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
 
-    if (pcl::io::loadPCDFile<pcl::PointXYZ>("/home/andrewlauer/Documents/projects/Slam-Dunk/CSite1_orig-utm.pcd", *cloud) == -1) {
+    
+    if (pcl::io::loadPCDFile<pcl::PointXYZ>("/home/andrewlauer/Documents/projects/Slam-Dunk/pointclouds/cloud_0000000000.pcd", *cloud) == -1) {
         // Error handling if the file doesn't exist or is not in the correct format
         PCL_ERROR("Could not read PCD file.\n");
         return -1;
@@ -24,12 +76,18 @@ int main() {
     //for (size_t i = 0; i < cloud->fields.size (); ++i) {
     //    std::cout << " " << cloud->fields[i].name;
     //}
-
+    
+    printf("1\n");
     pcl::visualization::PCLVisualizer::Ptr viewer(new pcl::visualization::PCLVisualizer("Cloud Viewer"));
+    //pcl::visualization::CloudViewer viewer("Simple Cloud Viewer");
+    printf("2\n");
 
     viewer->setBackgroundColor(0, 0, 0);
+    printf("3\n");
     pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> color_handler(cloud, 255, 255, 255);
+    printf("4\n");
     viewer->addPointCloud(cloud, color_handler, "cloud");
+    printf("5\n");
 
     //viewer.showCloud(cloud);
 
